14d471824b8ffbb479d0da9f36b59ad0ef749285
Ben Nesbit <benesbit@yahoo.com>
2020-02-15 16:31:05 -0800

    Add s3 info

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var s3 = new AWS.S3({
+    region: "es-east-1",
+    apiVersion: "2006-03-01",
+    params: { Bucket: "nesbit-music-app" }
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    
+    // res.send('Hello World, from express');
 });
 
 app.get('/shit', (req, res) => {
     res.send('this a test');
 
 });
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, () => console.log(`Music app listening on port ${port}!`))
